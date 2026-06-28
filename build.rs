0a9c9669cbf92049919e67ca5b4148b593d30adc
Joe <Joe>
2026-06-28 00:17:44 -0400

    Set Style and Position

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
-    slint_build::compile("ui/app.slint").expect("Slint build failed");
+    let config = slint_build::CompilerConfiguration::new().with_style("material-dark".into());
+
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,35 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::error::Error;
+use slint::LogicalPosition;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let ui = init_ui()?;
+
+    init_callbacks(&ui)?;
+
+    ui.run()?;
+
+    Ok(())
+}
+
+fn init_ui() -> Result<AppWindow, Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
+    ui.window().set_position(LogicalPosition::new(100 as f32, 100 as f32));
+
+    Ok(ui)
+}
+
+fn init_callbacks(ui: &AppWindow) -> Result<(), Box<dyn Error>> {
     let ui_handle = ui.as_weak();
+
     ui.on_request_increase_value(move || {
         let ui = ui_handle.unwrap();
         ui.set_counter(ui.get_counter() + 1);
     });
-
-    ui.run()?;
 
     Ok(())
 }
